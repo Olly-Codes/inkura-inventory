04e2873c414d76e20990724fee8bbed82c5e9772
Olly <ollycodesdev@gmail.com>
2026-06-02 10:34:21 +0200

    feat: add title param to index page

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -3,7 +3,7 @@ const db = require("../db/queries");
 exports.productsListGet = async (req, res, next) => {
     try {
         const products = await db.getAllProducts();
-        res.render("index", { products})
+        res.render("index", {title: "Home", products})
     } catch (err) {
         next(err)
     }
