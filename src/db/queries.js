282f05d31d7d8ec670188d0e44d25e125089223d
Olly <ollycodesdev@gmail.com>
2026-06-02 10:32:42 +0200

    feat: add query to get a single product

--- src/db/queries.js
+++ src/db/queries.js
@@ -12,6 +12,20 @@ const getAllProducts = async () => {
     return rows;
 }
 
+const getProduct = async (product_id) => {
+    const { rows } = await pool.query(`
+            SELECT * FROM
+            products p JOIN categories c
+            ON p.category_id = c.category_id
+            JOIN authors a
+            ON p.author_id = a.author_id
+            WHERE p.product_id = $1;
+        `, [product_id]
+    );
+    return rows[0]
+}
+
 module.exports = {
-    getAllProducts
+    getAllProducts,
+    getProduct
 }
