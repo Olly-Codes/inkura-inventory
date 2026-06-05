af30df5160940f7b602287dd8daed1fee77509a6
Olly <ollycodesdev@gmail.com>
2026-06-05 12:28:59 +0200

    feat: add and export query to get filtered products

--- src/db/queries.js
+++ src/db/queries.js
@@ -25,7 +25,35 @@ const getProduct = async (product_id) => {
     return rows[0]
 }
 
+const getFilteredProducts = async ({ categories, sortOrder, maxPrice }) => {
+    let query = `
+        SELECT * FROM products p
+        JOIN categories c ON p.category_id = c.category_id
+        JOIN authors a ON p.author_id = a.author_id
+    `;
+    const values = [];
+
+    if (categories && categories.length > 0) {
+        const placeholders = categories.map((_, index) => `$${index + 1}`).join(", ");
+        query += ` WHERE c.category_name IN (${placeholders})`;
+        values.push(...categories);
+    }
+
+    if (maxPrice) {
+        query += values.length > 0 ? ` AND` : ` WHERE`;
+        values.push(maxPrice);
+        query += ` p.product_price <= $${values.length}`;
+    }
+
+    const order = sortOrder === "desc" ? "DESC" : "ASC";
+    query += ` ORDER BY p.product_price ${order}`;
+
+    const { rows } = await pool.query(query, values);
+    return rows;
+};
+
 module.exports = {
     getAllProducts,
-    getProduct
+    getProduct,
+    getFilteredProducts
 }
