aa72cfb33714db47ced9b820a718473aac0b9d65
Olly <ollycodesdev@gmail.com>
2026-06-06 12:00:12 +0200

    feat: add product urls to views and setting static folder

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const detailsRouter = require("./routes/detailsRouter");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
