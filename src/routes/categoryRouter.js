aed7216f797a395c86ed4e7da63809603b6cdd75
Olly <ollycodesdev@gmail.com>
2026-06-06 13:42:49 +0200

    feat: add form and new category route and link to controllers

--- src/routes/categoryRouter.js
+++ src/routes/categoryRouter.js
@@ -4,5 +4,7 @@ const categoryController = require("../controllers/categoryController");
 const categoryRouter = Router();
 
 categoryRouter.get("/", categoryController.categoriesListGet);
+categoryRouter.get("/form", categoryController.newCategoryGet);
+categoryRouter.post("/", categoryController.newCategoryPost);
 
 module.exports = categoryRouter
