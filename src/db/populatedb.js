795b199fec43c2c3bef856aafbabe9c0d3b0af29
Olly <ollycodesdev@gmail.com>
2026-06-06 12:04:08 +0200

    refactor: changed how product titles are represented in database

--- src/db/populatedb.js
+++ src/db/populatedb.js
@@ -47,13 +47,13 @@ VALUES
 
 INSERT INTO products (product_name, product_description, product_quantity, product_price, product_image_url, category_id, author_id)
 VALUES
-    ('Naruto', 'An orphaned ninja with a sealed demon fox fights for acceptance and strives to become the ultimate village leader.', 7, 435.00, '/images/naruto.jpg', 2, 1),
-    ('Berserk', 'A lone mercenary wields a massive sword against demons and a tragic destiny in a dark, brutal fantasy world.', 25, 375.00, '/images/berserk.jpg', 4, 2),
-    ('Vagabond', 'A wandering swordsman battles ruthelss rivals and internal demonds on a brutal path to becoming a samurai legend.', 75,500.00, '/images/vagabond.jpg', 4, 3),
-    ('One Piece', 'A rubber powered pirate captain and his loyal crew sail treacherous seas to find the ultimate world treasure', 60, 623.00, '/images/one-piece.jpg', 2, 4),
-    ('Pretty Cure', 'Ordinary schoolgirls transform into legendary magical warriors to protect the universe from evil dark forces.', 5, 150.00, '/images/pretty-cure.jpg', 1, 5),
-    ('Sailor Moon', 'A clumsy schoolgirl awakens as a magical guardian to lead a team of heroins against cosmic evil forces', 45, 302.00, '/images/sailor-moon.jpg', 3, 6),
-    ('Princess Jellyfish', 'An introverted jellyfish-obsessed girl finds her life upended when a stylsh, cross-dressing fashinista enters her nerdy communal home.', 105, 922.00, '/images/princess-jellyfish.jpg', 5, 7);   
+    ('naruto', 'An orphaned ninja with a sealed demon fox fights for acceptance and strives to become the ultimate village leader.', 7, 435.00, '/images/naruto.jpg', 2, 1),
+    ('berserk', 'A lone mercenary wields a massive sword against demons and a tragic destiny in a dark, brutal fantasy world.', 25, 375.00, '/images/berserk.jpg', 4, 2),
+    ('vagabond', 'A wandering swordsman battles ruthelss rivals and internal demonds on a brutal path to becoming a samurai legend.', 75,500.00, '/images/vagabond.jpg', 4, 3),
+    ('one-piece', 'A rubber powered pirate captain and his loyal crew sail treacherous seas to find the ultimate world treasure', 60, 623.00, '/images/one-piece.jpg', 2, 4),
+    ('pretty-cure', 'Ordinary schoolgirls transform into legendary magical warriors to protect the universe from evil dark forces.', 5, 150.00, '/images/pretty-cure.jpg', 1, 5),
+    ('sailor-moon', 'A clumsy schoolgirl awakens as a magical guardian to lead a team of heroins against cosmic evil forces', 45, 302.00, '/images/sailor-moon.jpg', 3, 6),
+    ('princess-jellyfish', 'An introverted jellyfish-obsessed girl finds her life upended when a stylsh, cross-dressing fashinista enters her nerdy communal home.', 105, 922.00, '/images/princess-jellyfish.jpg', 5, 7);   
 
 `;
 
