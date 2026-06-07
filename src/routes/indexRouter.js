e43f1fe3162d325af6ed9d1bbb6a1d5e965b1b12
Olly <ollycodesdev@gmail.com>
2026-06-07 13:31:01 +0200

    feat: add activePage property to views

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -5,7 +5,7 @@ const indexRouter = Router();
 
 indexRouter.get("/", indexController.productsFiltertGet);
 
-indexRouter.get("/about", (req, res) => res.render("about", {title: "About" }));
-indexRouter.get("/contact", (req, res) => res.render("contact", {title: "Contact" }));
+indexRouter.get("/about", (req, res) => res.render("about", {title: "About", activePage: "about" }));
+indexRouter.get("/contact", (req, res) => res.render("contact", {title: "Contact", activePage: "contact" }));
 
 module.exports = indexRouter
