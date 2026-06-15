6a2fc3e9b7406ac3347dff4ccc011180a312d923
Olly <ollycodesdev@gmail.com>
2026-06-15 13:20:08 +0200

    feat: add logic for validation and user registration

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,29 @@
 const db = require("../config/db/queries");
+const bcrypt = require("bcrypt");
+const flash = require("express-flash");
 const { body, validationResult, matchedData } = require("express-validator");
+
+const validateRegistration = [
+    body("username").trim()
+        .notEmpty().withMessage("Username is required")
+        .isAlphanumeric().withMessage("Username must only contain letters and numbers")
+        .isLength({ min: 3, max: 30 }).withMessage("Username must be between 3 and 30 characters"),
+    body("email").trim()
+        .notEmpty().withMessage("Email is required")
+        .isEmail().withMessage("Must be a valid email")
+        .normalizeEmail(),
+    body("password")
+        .notEmpty().withMessage("Password is required")
+        .isLength({ min: 8 }).withMessage("Password must be at least 8 characters")
+        .isStrongPassword({ minUppercase: 1, minNumbers: 1, minSymbols: 1 }).withMessage("Password must at least contain one uppercase letter, number and symbol"),
+    body("password2").custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error("Passwords do not match");
+        }
+        return true;
+    })
+];
+
 
 exports.registerGet = async (req, res, next) => {
     try {
@@ -24,3 +48,40 @@ exports.dashboardGet = async (req, res, next) => {
         next(err);
     }
 }
+
+exports.registerPost = [
+    validateRegistration,
+    async (req, res, next) => {
+        const validationErrors = validationResult(req);
+        if (!validationErrors.isEmpty()) {
+            return res.status(400).render("auth/register", {
+                title: "Register",
+                activePage: "",
+                errors: validationErrors.array()
+            });
+        } else {
+            try {
+                const { username, email, password, password2 } = req.body;
+                const user = await db.getUserByEmail(email)
+
+                if (user) {
+                    res.render("auth/register", {
+                        title: "Register",
+                        activePage: "",
+                        errors: [{
+                            msg: "Email already registered"
+                        }]
+                    });
+                    return;
+                } else {
+                    const hashedPass = await bcrypt.hash(password, 10);
+                    await db.insertUser(username, email, hashedPass);
+                    req.flash("success_msg", "You have registred successfully. Please log in");
+                    res.redirect("/auth/login");
+                }
+            } catch (err) {
+                next(err)
+            }
+        }
+    }
+];
